ebd1cb42cce1b7f35a0108a62672627f9c1fdee8
joevaas <133934323+joevaas@users.noreply.github.com>
2025-02-23 16:34:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,14 +13,14 @@ import aiohttp
 URL = "https://inner-kristien-joevaas-5938480a.koyeb.app/"
 
 async def ping():
-async with aiohttp.ClientSession() as session:
-while True:
-try:
-async with session.get(URL) as response:
-print(f"Pinged server, status: {response.status}")
-except Exception as e:
-print(f"{e}")
-await asyncio.sleep(600)
+    async with aiohttp.ClientSession() as session:
+        while True:
+            try:
+                async with session.get(URL) as response:
+                    print(f"Pinged server, status: {response.status}")
+            except Exception as e:
+                print(f"{e}")
+            await asyncio.sleep(600)
 
 loop = asyncio.get_event_loop()
 loop.create_task(ping())
