aea5141503fb125855fceaa8f6d63f7659f36831
joevaas <133934323+joevaas@users.noreply.github.com>
2025-02-23 16:09:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from bot import Bot
 
 Bot().run()
+import asyncio
+import aiohttp
+
+URL = "https://inner-kristien-joevaas-5938480a.koyeb.app/"  # Replace with your koyeb app link...
+
+async def ping():
+    async with aiohttp.ClientSession() as session:
+        while True:
+            try:
+                async with session.get(URL) as response:
+                    print(f"Pinged server, status: {response.status}")
+            except Exception as e:
+                print(f"{e}")
+            await asyncio.sleep(600)
+
+loop = asyncio.get_event_loop()
+loop.create_task(ping())
